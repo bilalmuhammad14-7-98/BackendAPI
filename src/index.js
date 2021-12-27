4409ebc54c2f34609018ec7a233182535b6399e6
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2021-12-27 17:36:22 +0500

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0.itrdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+
+mongoose.connection.on("errot", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to Pakistan");
